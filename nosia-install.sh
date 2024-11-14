448a54498791109eb9daa3fa3e1601383ca44214
Cyril Blaecke <1692273+cbldev@users.noreply.github.com>
2024-11-14 09:34:19 +0000

    Refactor

--- nosia-install.sh
+++ nosia-install.sh
@@ -5,6 +5,17 @@ if ! [ -x "$(command -v git)" ]; then
 fi
 
 git clone https://github.com/nosia-ai/nosia.git
+
 cd ./nosia/
-OLLAMA_BASE_URL=$OLLAMA_BASE_URL LLM_MODEL=$LLM_MODEL EMBEDDING_MODEL=$EMBEDDING_MODEL CHECK_MODEL=$CHECK_MODEL ./script/production/setup
- ./script/production/start
+
+if ! [ -n "$OLLAMA_BASE_URL" ]; then
+  OLLAMA_BASE_URL=http://localhost:11434
+fi
+
+if ! [ -n "$LLM_MODEL" ]; then
+  LLM_MODEL=qwen2.5
+fi
+
+OLLAMA_BASE_URL=$OLLAMA_BASE_URL LLM_MODEL=$LLM_MODEL ./script/setup
+
+./script/start
