4d694e489bfa3760aeaeab4a83dc8439bf0159a8
Cyril Blaecke <1692273+cbldev@users.noreply.github.com>
2025-10-07 22:34:00 +0000

    Simplify

--- nosia-install.sh
+++ nosia-install.sh
@@ -8,14 +8,6 @@ git clone https://github.com/nosia-ai/nosia.git
 
 cd ./nosia/
 
-if ! [ -n "$AI_API_BASE" ]; then
-  AI_API_BASE=http://localhost:11434/v1
-fi
-
-if ! [ -n "$LLM_MODEL" ]; then
-  LLM_MODEL=granite4:micro-h
-fi
-
-AI_API_BASE=$AI_API_BASE LLM_MODEL=$LLM_MODEL ./script/setup
+LLM_MODEL=$LLM_MODEL ./script/setup
 
 ./script/start
